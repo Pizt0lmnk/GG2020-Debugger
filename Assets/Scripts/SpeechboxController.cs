f2e5d79842e5c9a7e615ef02ca8aa0a32df1ffae
Lamine Dia <lamine.dia@visuellverstehen.de>
2020-02-01 23:43:49 +0100

    Add speechbox

---
+++ Assets/Scripts/SpeechboxController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeechboxController : MonoBehaviour
+{
+    public Text textField;
+    public Image image;
+    public Sprite sprite1;
+    public Sprite sprite2;
+    private int counter = 0;
+    private string fullText;
+
+    void Start()
+	{
+        image.sprite = sprite1;
+        fullText = textField.text;
+        textField.text = "";
+        InvokeRepeating("TypeText", 0, 0.1f);
+    }
+
+    void TypeText() 
+    {
+        if (image.sprite == sprite1) {
+            image.sprite = sprite2;
+        } else {
+            image.sprite = sprite1;
+        }
+
+        textField.text += fullText.Substring(counter, 1);
+        if (textField.text == fullText) {
+            CancelInvoke("TypeText");
+            Invoke("hideSpeechbox", 3);
+        }
+        counter++;
+    }
+
+    public void showSpeechbox() 
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void hideSpeechbox()
+    {
+        gameObject.SetActive(false);
+    }
+}
